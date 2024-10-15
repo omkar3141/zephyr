b9cb2a2818dde1c301af81b0ade0ebd0204cd60d
Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>
2024-10-15 10:26:21 +0200

    WIP
    Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

--- samples/bluetooth/mesh/src/board.h
+++ samples/bluetooth/mesh/src/board.h
@@ -10,6 +10,6 @@ void board_output_number(bt_mesh_output_action_t action, uint32_t number);
 
 void board_prov_complete(void);
 
-int board_init(struct k_work *button_work);
+int board_init(struct k_work *button_work, struct k_work *button1_work);
 
 void board_led_set(bool val);
