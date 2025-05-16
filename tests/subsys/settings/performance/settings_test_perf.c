f5ebeea1c5afbd8146e382733b26efe369cb460b
Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>
2025-05-16 12:51:07 +0200

    Bluetooth: Test: ZMS + Settings + Scanning test
    Adds scanning functionality to test case to show comparision between
    packet loss when ZMS or NVS is not in use to dump large amount data
    into non volatile memory.
    
    Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

--- tests/subsys/settings/performance/settings_test_perf.c
+++ tests/subsys/settings/performance/settings_test_perf.c
@@ -18,16 +18,19 @@
 static struct k_work_q settings_work_q;
 static K_THREAD_STACK_DEFINE(settings_work_stack, 2024);
 static struct k_work_delayable pending_store;
+static uint32_t scanned_pkts;
+static bool bt_init_done;
 
 #define TEST_SETTINGS_COUNT      (128)
-#define TEST_STORE_ITR           (5)
+#define TEST_STORE_ITR           (15)
 #define TEST_TIMEOUT_SEC         (60)
 #define TEST_SETTINGS_WORKQ_PRIO (1)
 
 static void bt_scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
 		       struct net_buf_simple *buf)
 {
-	printk("len %u\n", buf->len);
+	// printk("len %u\n", buf->len);
+	scanned_pkts++;
 }
 
 struct test_setting {
@@ -45,11 +48,15 @@ static void store_pending(struct k_work *work)
 		uint32_t total_measured;
 		uint32_t single_entry_max;
 		uint32_t single_entry_min;
-	} stats = {0, 0, 0, UINT32_MAX};
+		uint32_t single_itr_max;
+		uint32_t single_itr_min;
+	} stats = {0, 0, 0, UINT32_MAX, 0, UINT32_MAX};
 
 	int64_t ts1 = k_uptime_get();
 	/* benchmark storage performance */
 	for (int j = 0; j < TEST_STORE_ITR; j++) {
+		int64_t ts_itr = k_uptime_get();
+
 		for (int i = 0; i < TEST_SETTINGS_COUNT; i++) {
 			test_settings[i].val = TEST_SETTINGS_COUNT * j + i;
 
@@ -69,6 +76,15 @@ static void store_pending(struct k_work *work)
 				stats.single_entry_min = delta2;
 			}
 			stats.total_calculated += delta2;
+		}
+
+		int64_t delta_itr = k_uptime_delta(&ts_itr);
+
+		if (stats.single_itr_max < delta_itr) {
+			stats.single_itr_max = delta_itr;
+		}
+		if (stats.single_itr_min > delta_itr) {
+			stats.single_itr_min = delta_itr;
 		}
 	}
 
@@ -101,6 +117,8 @@ static void store_pending(struct k_work *work)
 	printk("total calculated: %u, total measured: %u\n", stats.total_calculated,
 	       stats.total_measured);
 	printk("entry max: %u, entry min: %u\n", stats.single_entry_max, stats.single_entry_min);
+	printk("itr max: %u, itr min: %u\n", stats.single_itr_max, stats.single_itr_min);
+	printk("scanned packets: %u, pkts per sec: %d\n", scanned_pkts, (int) (scanned_pkts / (stats.total_measured/1000.0)));
 
 	k_sem_give(&waitfor_work);
 }
@@ -110,6 +128,8 @@ ZTEST_SUITE(settings_perf, NULL, NULL, NULL, NULL, NULL);
 ZTEST(settings_perf, test_performance)
 {
 	int err;
+
+	scanned_pkts = 0;
 
 	if (IS_ENABLED(CONFIG_NVS)) {
 		printk("Testing with NVS\n");
@@ -125,8 +145,10 @@ ZTEST(settings_perf, test_performance)
 
 	if (IS_ENABLED(CONFIG_BT)) {
 		/* enable one of the major subsystems, and start scanning. */
-		err = bt_enable(NULL);
-		zassert_equal(err, 0, "Bluetooth init failed (err %d)\n", err);
+		if (!bt_init_done) {
+			err = bt_enable(NULL);
+			zassert_equal(err, 0, "Bluetooth init failed (err %d)\n", err);
+		}
 
 		err = bt_le_scan_start(BT_LE_SCAN_ACTIVE, bt_scan_cb);
 		zassert_equal(err, 0, "Scanning failed to start (err %d)\n", err);
@@ -149,4 +171,43 @@ ZTEST(settings_perf, test_performance)
 		err = bt_le_scan_stop();
 		zassert_equal(err, 0, "Scanning failed to stop (err %d)\n", err);
 	}
+}
+
+
+ZTEST(settings_perf, test_only_scan)
+{
+	int err;
+
+	scanned_pkts = 0;
+
+	printk("Only scanning\n");
+	if (IS_ENABLED(CONFIG_NVS)) {
+		printk("Testing with NVS, but no actual writes\n");
+	} else if (IS_ENABLED(CONFIG_ZMS)) {
+		printk("Testing with ZMS, but no actual writes\n");
+	}
+
+	if (IS_ENABLED(CONFIG_BT)) {
+		/* enable one of the major subsystems, and start scanning. */
+		if (!bt_init_done) {
+			err = bt_enable(NULL);
+			zassert_equal(err, 0, "Bluetooth init failed (err %d)\n", err);
+			bt_init_done = true;
+		}
+
+		err = bt_le_scan_start(BT_LE_SCAN_ACTIVE, bt_scan_cb);
+		zassert_equal(err, 0, "Scanning failed to start (err %d)\n", err);
+	}
+
+	err = settings_subsys_init();
+	zassert_equal(err, 0, "settings_backend_init failed %d", err);
+
+	k_sleep(K_SECONDS(10));
+
+	if (IS_ENABLED(CONFIG_BT)) {
+		err = bt_le_scan_stop();
+		zassert_equal(err, 0, "Scanning failed to stop (err %d)\n", err);
+	}
+
+	printk("scanned packets: %u, pkts per sec: %d\n", scanned_pkts, (int)(scanned_pkts / 10.0));
 }
